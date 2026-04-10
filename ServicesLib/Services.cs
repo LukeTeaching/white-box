312e78d3e6f2cbbed3c14fb5a1bd71568440767c
Luke <npv.loc@gmail.com>
2026-04-11 03:34:47 +0700

    Init action

--- ServicesLib/Services.cs
+++ ServicesLib/Services.cs
@@ -9,5 +9,27 @@
             if (score >= 5) return "Pass";                
             return "Fail";                                
         }
+
+        public string ProcessGrades(double[] grades)
+        {
+            if (grades.Length == 0) return "Empty List";
+
+            double sum = 0;
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (grades[i] < 0 || grades[i] > 10)
+                {
+                    return "Invalid Grade";
+                }
+                sum += grades[i];
+            }
+
+            double avg = sum / grades.Length;
+
+            if (avg >= 8.0) return "Excellent";
+            if (avg >= 5.0) return "Passed";
+
+            return "Failed";
+        }
     }
 }
